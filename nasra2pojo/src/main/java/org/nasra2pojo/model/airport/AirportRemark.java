2e33f78c2418d87e5c66c7fe6316dcf71328c6b4
Jakob Stuber <skajak@gmail.com>
2016-01-10 21:39:09 -0600

    Remark attributes

--- nasra2pojo/src/main/java/org/nasra2pojo/model/airport/AirportRemark.java
+++ nasra2pojo/src/main/java/org/nasra2pojo/model/airport/AirportRemark.java
@@ -1,8 +1,32 @@
 package org.nasra2pojo.model.airport;
 
+import com.github.ffpojo.metadata.positional.annotation.PositionalField;
 import com.github.ffpojo.metadata.positional.annotation.PositionalRecord;
 
 @PositionalRecord
 public class AirportRemark extends AirportBase {
 
+	private String name;
+	private String remark;
+	
+	@PositionalField(initialPosition = 17, finalPosition = 29)
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	@PositionalField(initialPosition = 30, finalPosition = 1529)
+	public String getRemark() {
+		return remark;
+	}
+	public void setRemark(String remark) {
+		this.remark = remark;
+	}
+	
+	@Override
+	public String toString() {
+		return "AirportRemark [name=" + name + ", remark=" + remark + "]";
+	}
 }
